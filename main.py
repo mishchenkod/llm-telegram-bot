ec999f0ce96f0da6d8f78baab6416b79d43e077b
Dzmitry Mishchanka <dzmitry.mishchanka@gmail.com>
2025-02-16 20:08:39 +0100

    Setup Poetry project

---
+++ main.py
@@ -0,0 +1,43 @@
+import asyncio
+import logging
+import os
+
+from telegram import Update
+from telegram.ext import (
+    ApplicationBuilder,
+    CallbackContext,
+    CommandHandler,
+    MessageHandler,
+    filters,
+)
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+
+async def start(update: Update, context: CallbackContext) -> None:
+    """Send a welcome message when the /start command is issued."""
+    await update.message.reply_text("Hello! I'm your Telegram bot.")
+
+
+async def echo(update: Update, context: CallbackContext):
+    """Echo user messages."""
+    await update.message.reply_text(update.message.text)
+
+
+def main():
+    """Start the bot."""
+    app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    print("Bot is running...")
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
